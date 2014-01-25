e905644be6c38c98f936dab6d191f828d8205d59
Blazej Marcinkiewicz <blazej.marcinkiewicz@polidea.com>
2014-01-25 16:26:28 +0100

    Added automove + spawn object scripts

---
+++ Polyjam/Assets/SpawnObjects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour {
+
+	float rate = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time % rate < 0.02) {
+			print("Spawn");
+			GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			go.transform.parent = transform;
+			go.transform.localPosition = new Vector3(Random.Range(0, transform.localScale.x),
+			                                    0, Random.Range(0, transform.localScale.z));
+			PlayerAI pai = go.AddComponent("PlayerAI") as PlayerAI;
+			pai.speed = Random.Range(1, 20) * 0.1f;
+		}
+	}
+}
