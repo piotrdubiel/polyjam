019400084d39b19d1d830253dd2a8755c6ea1b71
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-01-25 13:59:47 +0100

    init

---
+++ Polyjam/Assets/GenePanel/PanelBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelBehaviour : MonoBehaviour {
+	public GUITexture panel;
+
+	// Use this for initialization
+	void Start () {
+		panel = GetComponent<GUITexture> ();
+
+		transform.position = Vector3.zero;
+		transform.localScale = Vector3.zero;
+		panel.pixelInset = new Rect(0, 0, Screen.width / 2, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onGUI() {
+		if (GUI.Button (new Rect (10,10,150,100), "I am a button")) {
+			print ("You clicked the button!");
+		}
+	}
+}
