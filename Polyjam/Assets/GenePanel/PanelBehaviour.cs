b37722452c71ec2631e8679d915fd21f937b3aeb
Piotrek Dubiel <piotr.dubiel@polidea.com>
2014-01-25 14:10:53 +0100

    panel fix

--- Polyjam/Assets/GenePanel/PanelBehaviour.cs
+++ Polyjam/Assets/GenePanel/PanelBehaviour.cs
@@ -6,8 +6,6 @@ public class PanelBehaviour : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		panel = GetComponent<GUITexture> ();
-
 		transform.position = Vector3.zero;
 		transform.localScale = Vector3.zero;
 		panel.pixelInset = new Rect(0, 0, Screen.width / 2, Screen.height);
@@ -19,6 +17,11 @@ public class PanelBehaviour : MonoBehaviour {
 	}
 
 	void onGUI() {
+		GUI.Box (new Rect (0,0,100,50), "Top-left");
+		GUI.Box (new Rect (Screen.width - 100,0,100,50), "Top-right");
+		GUI.Box (new Rect (0,Screen.height - 50,100,50), "Bottom-left");
+		GUI.Box (new Rect (Screen.width - 100,Screen.height - 50,100,50), "Bottom-right");
+
 		if (GUI.Button (new Rect (10,10,150,100), "I am a button")) {
 			print ("You clicked the button!");
 		}
