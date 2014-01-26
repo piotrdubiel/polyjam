29a7386de9c683cf9aa814a16b1893712d340931
Blazej Marcinkiewicz <blazej.marcinkiewicz@polidea.com>
2014-01-26 16:49:49 +0100

    Game quit

--- Polyjam/Assets/GameOverGUI.cs
+++ Polyjam/Assets/GameOverGUI.cs
@@ -11,15 +11,17 @@ public class GameOverGUI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space)) {
-			print ("restart");
-			MockStats.fangs = 0;
-			MeatBehaviour.Food = 10;
-			MeatBehaviour.Points = 100;
-			PlantBehaviour.Food = 5;
-			PlantBehaviour.Points = 100;
-			Application.LoadLevel("sample");
+		if (Input.GetKeyDown (KeyCode.Space)) {
+				print ("restart");
+				MockStats.fangs = 0;
+				MeatBehaviour.Food = 10;
+				MeatBehaviour.Points = 100;
+				PlantBehaviour.Food = 5;
+				PlantBehaviour.Points = 100;
+				Application.LoadLevel ("sample");
 
+		} else if (Input.GetKeyDown (KeyCode.Q)) {
+			Application.Quit();
 		}
 	}
 }
